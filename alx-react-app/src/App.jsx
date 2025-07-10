2c8eb51cf141b77903b44fe86cd9d33f30bd1f48
MudMoh <mudesir@gmail.com>
2025-07-10 22:16:04 +0300

    the userprofile component added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/footer";
+import UserProfile from "./components/UserProfile";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -23,6 +24,7 @@ function App() {
 				<Header />
 				<MainContent />
 				<Footer />
+				<UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
