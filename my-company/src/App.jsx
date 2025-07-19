c58a29498c3317be62ec0c1a2854b1cf2f5075e6
MudMoh <mudesir@gmail.com>
2025-07-19 19:37:30 +0300

    a new react webpage

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -28,19 +28,3 @@ function App() {
 }
 
 export default App;
-
-function Home() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <img
-        src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80"
-        alt="Company"
-        style={{ width: "60%", borderRadius: "12px", marginBottom: "24px" }}
-      />
-      <h1>Welcome to Our Company</h1>
-      <p>We are dedicated to delivering excellence in all our services.</p>
-    </div>
-  );
-}
-
-export default Home;
