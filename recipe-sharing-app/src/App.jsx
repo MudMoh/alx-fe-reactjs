c6451b8f09f70911e50475a64a593fb972711a2c
MudMoh <mudesir@gmail.com>
2025-07-24 00:51:12 +0300

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import Router from "./components/Router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import AddRecipeForm from "./components/AddRecipeForm";
 import "./App.css";
-
 function App() {
 	return (
 		<>
 			<h1>Recipe Sharing App</h1>
-			<div>
-				<Router />
-			</div>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<div>
+								<AddRecipeForm />
+								<RecipeList />
+							</div>
+						}
+					/>
+					<Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+					<Route path="/add-recipe" element={<AddRecipeForm />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
