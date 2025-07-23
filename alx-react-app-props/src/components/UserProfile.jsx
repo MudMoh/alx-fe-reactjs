c593bb1d808009571b99d02ef81906c7bda8e798
MudMoh <mudesir@gmail.com>
2025-07-23 14:00:55 +0300

    userProfile.jsx updated according to the requiremetn

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-const UserProfile = (props) => {
+import React from "react";
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () => {
+	const userData = useContext(UserContext);
+
 	return (
 		<div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-			<h2 style={{ color: "blue" }}>{props.name}</h2>
+			<h2 style={{ color: "blue" }}>{userData.name}</h2>
 			<p>
-				Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
+				Age: <span style={{ fontWeight: "bold" }}>{userData.age}</span>
 			</p>
-			<p>Bio: {props.bio}</p>
+			<p>Bio: {userData.bio}</p>
 		</div>
 	);
 };
