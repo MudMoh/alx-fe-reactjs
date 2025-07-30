c1a8c9d51dcb2577046d696e729f3d7af0010724
MudMoh <mudesir@gmail.com>
2025-07-30 16:13:30 +0300

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,27 +1,18 @@
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com';
-const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
-
-const github = axios.create({
-  baseURL: GITHUB_API_URL,
-  headers: {
-    Authorization: `token ${GITHUB_API_KEY}`,
-  },
-});
-
 export const fetchUserData = (query, location, minRepos) => {
-  const searchParams = new URLSearchParams({
-    q: query,
-  });
-
+  let url = 'https://api.github.com/search/users?q=';
+  let q = query;
   if (location) {
-    searchParams.set('q', `${searchParams.get('q')}+location:${location}`);
+    q += `+location:${location}`;
   }
-
   if (minRepos) {
-    searchParams.set('q', `${searchParams.get('q')}+repos:>=${minRepos}`);
+    q += `+repos:>=${minRepos}`;
   }
-
-  return github.get(`/search/users?${searchParams.toString()}`);
+  url += q;
+  return axios.get(url, {
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+    },
+  });
 };
